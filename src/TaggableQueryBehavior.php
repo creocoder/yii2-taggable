a113ba8793b313ecf4184aaa9988617ea080c7c6
Alexander Kochetov <creocoder@gmail.com>
2015-01-16 23:45:54 +0300

    Added TaggableQueryBehavior

---
+++ src/TaggableQueryBehavior.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @link https://github.com/creocoder/yii2-taggable
+ * @copyright Copyright (c) 2015 Alexander Kochetov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace creocoder\taggable;
+
+use yii\base\Behavior;
+
+/**
+ * TaggableQueryBehavior
+ *
+ * @property \yii\db\ActiveQuery $owner
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class TaggableQueryBehavior extends Behavior
+{
+    /**
+     * @param string|string[] $names
+     * @return \yii\db\ActiveQuery the owner
+     */
+    public function anyTagNames($names)
+    {
+        $model = new $this->owner->modelClass();
+        $tagClass = $model->getRelation($model->tagRelation)->modelClass;
+
+        $this->owner
+            ->innerJoinWith($model->tagRelation, false)
+            ->andWhere([$tagClass::tableName() . '.' . $model->tagNameAttribute => $model->filterTagNames($names)])
+            ->addGroupBy(array_map(function ($pk) use ($model) { return $model->tableName() . '.' . $pk; }, $model->primaryKey()));
+
+        return $this->owner;
+    }
+}
