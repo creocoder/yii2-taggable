1379307e0233177a9a9613927ef5b88f44afdc7a
Alexander Kochetov <creocoder@gmail.com>
2015-01-16 17:16:09 +0300

    Added new cases to unit tests

--- tests/TaggableBehaviorTest.php
+++ tests/TaggableBehaviorTest.php
@@ -53,6 +53,21 @@ class TaggableBehaviorTest extends DatabaseTestCase
         $this->assertDataSetsEqual($expectedDataSet, $dataSet);
     }
 
+    public function testCreatePostSetTagNamesAsArray()
+    {
+        $post = new Post([
+            'title' => 'New post title',
+            'body' => 'New post body',
+            'tagNames' => ['tag4', 'tag4', 'tag5', '', 'tag6'],
+        ]);
+
+        $this->assertTrue($post->save());
+
+        $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
+        $expectedDataSet = $this->createFlatXMLDataSet(__DIR__ . '/data/test-create-post-with-tags.xml');
+        $this->assertDataSetsEqual($expectedDataSet, $dataSet);
+    }
+
     public function testCreatePostAddTagNames()
     {
         $post = new Post([
@@ -61,6 +76,21 @@ class TaggableBehaviorTest extends DatabaseTestCase
         ]);
 
         $post->addTagNames('tag4, tag4, tag5, , tag6');
+        $this->assertTrue($post->save());
+
+        $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
+        $expectedDataSet = $this->createFlatXMLDataSet(__DIR__ . '/data/test-create-post-with-tags.xml');
+        $this->assertDataSetsEqual($expectedDataSet, $dataSet);
+    }
+
+    public function testCreatePostAddTagNamesAsArray()
+    {
+        $post = new Post([
+            'title' => 'New post title',
+            'body' => 'New post body',
+        ]);
+
+        $post->addTagNames(['tag4', 'tag4', 'tag5', '', 'tag6']);
         $this->assertTrue($post->save());
 
         $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
@@ -93,12 +123,38 @@ class TaggableBehaviorTest extends DatabaseTestCase
         $this->assertDataSetsEqual($expectedDataSet, $dataSet);
     }
 
+    public function testUpdatePostSetTagNamesAsArray()
+    {
+        $post = Post::findOne(2);
+        $post->title = 'Updated post title 2';
+        $post->body = 'Updated post body 2';
+        $post->tagNames = ['tag3', 'tag3', 'tag4', '', 'tag6'];
+        $this->assertTrue($post->save());
+
+        $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
+        $expectedDataSet = $this->createFlatXMLDataSet(__DIR__ . '/data/test-update-post-with-tags.xml');
+        $this->assertDataSetsEqual($expectedDataSet, $dataSet);
+    }
+
     public function testUpdatePostAddTagNames()
     {
         $post = Post::findOne(2);
         $post->title = 'Updated post title 2';
         $post->body = 'Updated post body 2';
         $post->addTagNames('tag3, tag3, tag4, , tag6');
+        $this->assertTrue($post->save());
+
+        $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
+        $expectedDataSet = $this->createFlatXMLDataSet(__DIR__ . '/data/test-update-post-with-tags.xml');
+        $this->assertDataSetsEqual($expectedDataSet, $dataSet);
+    }
+
+    public function testUpdatePostAddTagNamesAsArray()
+    {
+        $post = Post::findOne(2);
+        $post->title = 'Updated post title 2';
+        $post->body = 'Updated post body 2';
+        $post->addTagNames(['tag3', 'tag3', 'tag4', '', 'tag6']);
         $this->assertTrue($post->save());
 
         $dataSet = $this->getConnection()->createDataSet(['post', 'tag', 'post_tag_assn']);
