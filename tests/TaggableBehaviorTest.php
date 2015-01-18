1bc5a7c470e6f5c17ab4cb819630fa4841a39389
Alexander Kochetov <creocoder@gmail.com>
2015-01-18 15:00:44 +0300

    Added new cases to unit tests

--- tests/TaggableBehaviorTest.php
+++ tests/TaggableBehaviorTest.php
@@ -192,6 +192,13 @@ class TaggableBehaviorTest extends DatabaseTestCase
         $this->assertDataSetsEqual($expectedDataSet, $dataSet);
     }
 
+    public function testPosHasTagNames()
+    {
+        $post = Post::findOne(2);
+        $this->assertTrue($post->hasTagNames('tag2, tag2, , tag4'));
+        $this->assertFalse($post->hasTagNames('tag3, tag3, , tag5'));
+    }
+
     public function testDeletePost()
     {
         $post = Post::findOne(2);
