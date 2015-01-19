ae9a64da295a4e72aa5ddd271ef940ab9ee598f9
a-komarev <ell@cybercog.su>
2015-01-20 02:08:01 +0300

    Added method to get related to model tags as array.

--- src/TaggableBehavior.php
+++ src/TaggableBehavior.php
@@ -65,6 +65,20 @@ class TaggableBehavior extends Behavior
     }
 
     /**
+     * @return array|string[]
+     */
+    public function getTagNamesArray()
+    {
+        if (!$this->owner->getIsNewRecord()
+            && $this->_tagNames === null
+            && !$this->owner->isRelationPopulated($this->tagRelation)) {
+            $this->populateTagNames();
+        }
+
+        return $this->_tagNames === null ? [] : $this->_tagNames;
+    }
+
+    /**
      * @param string|string[] $names
      */
     public function setTagNames($names)
