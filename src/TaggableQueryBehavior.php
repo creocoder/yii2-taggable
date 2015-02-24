b796df1b7484bdefc194d3783033e00747291aad
Alexander Kochetov <creocoder@gmail.com>
2015-02-24 21:03:58 +0300

    Refactored TaggableQueryBehavior

--- src/TaggableQueryBehavior.php
+++ src/TaggableQueryBehavior.php
@@ -21,17 +21,18 @@ class TaggableQueryBehavior extends Behavior
 {
     /**
      * Gets entities by any tags.
-     * @param string|string[] $names
+     * @param string|string[] $values
+     * @param string|null $attribute
      * @return \yii\db\ActiveQuery the owner
      */
-    public function anyTagNames($names)
+    public function anyTagValues($values, $attribute = null)
     {
         $model = new $this->owner->modelClass();
         $tagClass = $model->getRelation($model->tagRelation)->modelClass;
 
         $this->owner
             ->innerJoinWith($model->tagRelation, false)
-            ->andWhere([$tagClass::tableName() . '.' . $model->tagNameAttribute => $model->filterTagNames($names)])
+            ->andWhere([$tagClass::tableName() . '.' . ($attribute ?: $model->tagValueAttribute) => $model->filterTagValues($values)])
             ->addGroupBy(array_map(function ($pk) use ($model) { return $model->tableName() . '.' . $pk; }, $model->primaryKey()));
 
         return $this->owner;
@@ -39,23 +40,25 @@ class TaggableQueryBehavior extends Behavior
 
     /**
      * Gets entities by all tags.
-     * @param string|string[] $names
+     * @param string|string[] $values
+     * @param string|null $attribute
      * @return \yii\db\ActiveQuery the owner
      */
-    public function allTagNames($names)
+    public function allTagValues($values, $attribute = null)
     {
         $model = new $this->owner->modelClass();
 
-        return $this->anyTagNames($names)->andHaving(new Expression('COUNT(*) = ' . count($model->filterTagNames($names))));
+        return $this->anyTagValues($values, $attribute)->andHaving(new Expression('COUNT(*) = ' . count($model->filterTagValues($values))));
     }
 
     /**
      * Gets entities related by tags.
-     * @param string|string[] $names
+     * @param string|string[] $values
+     * @param string|null $attribute
      * @return \yii\db\ActiveQuery the owner
      */
-    public function relatedByTagNames($names)
+    public function relatedByTagValues($values, $attribute = null)
     {
-        return $this->anyTagNames($names)->addOrderBy(new Expression('COUNT(*) DESC'));
+        return $this->anyTagValues($values, $attribute)->addOrderBy(new Expression('COUNT(*) DESC'));
     }
 }
